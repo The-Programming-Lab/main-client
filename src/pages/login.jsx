01b58c765d66a48650781b6f8dd919eea2a7ebd2
BraedenHCL <braeden.norman6@gmail.com>
2023-04-13 22:45:36 -0700

    updated login file to test if ts was issue

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -3,14 +3,14 @@ import { getAuth, signInWithPopup, GithubAuthProvider } from 'firebase/auth';
 
 function App() {
   const firebaseConfig = {
-    apiKey: "AIzaSyBCdjPR5fIQLcpRe8yYKnp7r8mPnLp78_w",
-    authDomain: "the-programming-lab-d883c.firebaseapp.com",
-    databaseURL: "https://the-programming-lab-d883c-default-rtdb.firebaseio.com",
-    projectId: "the-programming-lab-d883c",
-    storageBucket: "the-programming-lab-d883c.appspot.com",
-    messagingSenderId: "8803580286",
-    appId: "1:8803580286:web:ce74d9065be3aaffffdb2f",
-    measurementId: "G-K2VSFCED77"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
   };
 
   const firebaseApp = initializeApp(firebaseConfig);
